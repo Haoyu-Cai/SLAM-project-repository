22a37ce2904c9be2a29b7d1d8ed591315cbe296d
cluster <859287553@qq.com>
2018-08-10 09:42:27 +0800

    Update trans_1.cpp

--- trans_1.cpp
+++ trans_1.cpp
@@ -11,19 +11,17 @@ public:
 
 VelodyneScan_Convert_LaserScan()
 {   
-    subscriber_1 = n.subscribe("/velodyne_packets",2000,&VelodyneScan_Convert_LaserScan::chatterCallback, this);
     publisher_1 = n.advertise<sensor_msgs::LaserScan>("/new_LaserScan_2d", 2000);
-    publisher_1.publish(cartographer_1);
+    subscriber_1 = n.subscribe("/velodyne_packets",2000,&VelodyneScan_Convert_LaserScan::chatterCallback, this);
+    
+   
 }
-
-
-
 
 void chatterCallback(const velodyne_msgs::VelodyneScan::ConstPtr& msg)
 {
    
   //ROS_INFO("%d %d %s", msg->header.seq, msg->header.stamp.nsec, msg->header.frame_id.c_str());
-  
+    sensor_msgs::LaserScan cartographer_1;
   cartographer_1.header.stamp.sec=msg->header.stamp.sec;
   cartographer_1.header.stamp.nsec=msg->header.stamp.nsec;
   cartographer_1.header.seq = msg->header.seq+1;
@@ -53,25 +51,47 @@ void chatterCallback(const velodyne_msgs::VelodyneScan::ConstPtr& msg)
                 if(flag==0)
                 {
 //                    cartographer_1->ranges[i*24+j]=float(msg->packets[i].data[100*j+6]);
+                        cartographer_1.intensities.push_back(float(msg->packets[i].data[6+100*j]));
  //                   cartographer_1.intensities[i*24+j*2+flag]=float(msg->packets[i].data[6+100*j]);
  //                   cartographer_1.ranges[i*24+j*2+flag]=(uint32_t(msg->packets[i].data[6+100*j+2]))<<8;
  //                   cartographer_1.ranges[i*24+j*2+flag]+=uint32_t(int(msg->packets[i].data[6+100*j+1]));
   //                  cartographer_1.ranges[i*24+j*2+flag]=TwoUint8_to_Float32(&(msg->packets[i].data[6+100*j+1]));
-                      int addr =msg->packets[i].data[6+100*j+1] & 0xFF;  
-                    addr |= msg->packets[i].data[6+100*j+2]<<8 & 0xFF00;  
-                     cartographer_1.ranges[i*24+j*2+flag]=addr; 
+                      int addr =int(msg->packets[i].data[6+100*j+1]) & 0xFF;  
+                    addr |= int(msg->packets[i].data[6+100*j+2])<<8 & 0xFF00;  
+                    float range_dis=float(addr)*2/1000;
+                    if(range_dis>200)
+                    {
+                    cartographer_1.ranges.push_back(cartographer_1.range_max)
+                    }
+           //          cartographer_1.ranges[i*24+j*2+flag]=float(addr)*2/1000;
+                    else{
+                     cartographer_1.ranges.push_back(range_dis);
+                    }
+  //                   cartographer_1.ranges[i*24+j*2+flag]=addr; 
                      //float strong convertion
                     flag++;
                 }
                 else if(flag==1)
                 {
- //                   cartographer_1.intensities[i*24+j*2+flag]=float(msg->packets[i].data[54+100*j]);
+ //                  cartographer_1.intensities[i*24+j*2+flag]=float(msg->packets[i].data[54+100*j]);
+                    cartographer_1.intensities.push_back(float(msg->packets[i].data[54+100*j]));
  //                   cartographer_1.ranges[i*24*j*2+flag]=(uint32_t(msg->packets[i].data[54+100*j+2]))<<8;
  //                   cartographer_1.ranges[i*24+j*2+flag]+=uint32_t(msg->packets[i].data[54+100*j+1]);
   //            cartographer_1.ranges[i*24+j*2+flag]=TwoUint8_to_Float32(&(msg->packets[i].data[54+100*j+1]));
-                    int addr =msg->packets[i].data[54+100*j+1] & 0xFF;  
-                     addr |= msg->packets[i].data[54+100*j+2]<<8 & 0xFF00;  
-                     cartographer_1.ranges[i*24+j*2+flag]=addr; 
+                    int addr =int(msg->packets[i].data[54+100*j+1]) & 0xFF;  
+                     addr |= int(msg->packets[i].data[54+100*j+2])<<8 & 0xFF00;  
+                     float range_dis=float(addr)*2/1000;
+
+                    if(range_dis>200)
+                    {
+                    cartographer_1.ranges.push_back(cartographer_1.range_max);
+                    }
+    //                cartographer_1.ranges[i*24+j*2+flag]=range_dis;
+                    else
+                    {
+                    cartographer_1.ranges.push_back(range_dis);
+                    }
+ //                    cartographer_1.ranges[i*24+j*2+flag]=addr; 
                      // float strong convertion
                     flag++;
                 }
@@ -80,7 +100,7 @@ void chatterCallback(const velodyne_msgs::VelodyneScan::ConstPtr& msg)
     }
 //  for(i=0;msg->packets[i].stamp.nsec<msg->header.stamp.nsec;i++);
 // ROS_INFO("%d %d %d", i,msg->packets[0].stamp.nsec,msg->packets[i].stamp.nsec);
-
+     publisher_1.publish(cartographer_1);
 }
 /*
 uint32_t TwoUint8_to_Uint32(const unsigned char* uint8s)
@@ -104,7 +124,7 @@ private:
     ros::NodeHandle n;
     ros::Publisher publisher_1;
     ros::Subscriber subscriber_1;
-    sensor_msgs::LaserScan cartographer_1;
+  
 };
 
 int main(int argc, char *argv[])
